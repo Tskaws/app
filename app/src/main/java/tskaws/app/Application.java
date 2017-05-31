ce81e802edabe10845aae18748b89a727831f639
jjreed05 <jjreed05@gmail.com>
2017-05-31 14:04:39 -0600

    Adding EventItem and Application

--- app/src/main/java/tskaws/app/Application.java
+++ app/src/main/java/tskaws/app/Application.java
@@ -4,12 +4,33 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import tskaws.app.EventItem;
+
 public class Application {
-	private List<Activity> activityList;
+	private List<com.tskaws.Activity> activityList;
 	public Application() {
 		this.activityList = new ArrayList<>();
 	}
 	public void start() throws IOException {
-		System.out.println(Crawler.getFeed());
+		System.out.println(com.tskaws.Crawler.getFeed());
+	}
+
+	private EventItem eventItems = new EventItem();
+	private String user;
+
+	public EventItem getEventItems() {
+		return eventItems;
+	}
+
+	public void setEventItems(EventItem eventItems) {
+		this.eventItems = eventItems;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
 	}
 }
---
+++ app/src/main/java/tskaws/app/EventItem.java
@@ -0,0 +1,83 @@
+package tskaws.app;
+
+/**
+ * Created by jj_re on 5/31/2017.
+ */
+
+public class EventItem {
+
+    public EventItem(){
+        this.title = "";
+        this.date = "";
+        this.description = "";
+        this.category = "";
+        this.link = "";
+    }
+
+    // Class member variables
+    private String title;
+    private String date;
+    private String description;
+    private String category;
+    private String link;
+
+    /*
+    private Object pictures1;
+    private Object pictures2;
+    */
+
+    // Getter for title
+    public String getTitle() {
+        return title;
+    }
+    // Setter for title
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    // Getter for description
+    public String getDescription() {
+        return description;
+    }
+    // Setter for description
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // Getter for Category
+    public String getCategory() {
+        return category;
+    }
+    // Setter for Category
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    // Getter for link
+    public String getLink() {
+        return link;
+    }
+    // Setter for link
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    /*  I commented these out until we make the object for pictures
+
+    public String getPictures1() {
+        return pictures1;
+    }
+
+    public void setPictures1(String pictures1) {
+        this.pictures1 = pictures1;
+    }
+
+    public String getPictures2() {
+        return pictures2;
+    }
+
+    public void setPictures2(String pictures2) {
+        this.pictures2 = pictures2;
+    }
+    */
+}
