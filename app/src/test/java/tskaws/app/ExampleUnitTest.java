1556d7129bea96fb4b3a9e6e32bdb013f71407f1
Ryan Allred <Ryan@Allred.xyz>
2017-06-07 13:22:28 -0600

    Some cool tests

--- app/src/test/java/tskaws/app/ExampleUnitTest.java
+++ app/src/test/java/tskaws/app/ExampleUnitTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +14,25 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void eventItemCreated() throws Exception {
+        EventItem item = new EventItem();
+        assertTrue(item instanceof EventItem);
+    }
+    @Test
+    public void eventItemSetters() throws Exception {
+        EventItem item = new EventItem();
+        item.setTitle("Cool Title");
+
+        assertTrue(item.getTitle().equals("Cool Title"));
+
+        item.setDescription("Cool Description");
+        assertTrue(item.getDescription().equals("Cool Description"));
+    }
+    @Test
+    public void crawlerCrawls() throws Exception {
+        List<EventItem> list = Crawler.crawl();
+        assertTrue(list.size() != 0);
     }
 }
