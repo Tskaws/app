f6fc329384859e5138d1690530f0ec40517ed871
jjreed05 <jjreed05@gmail.com>
2017-06-13 10:36:50 -0600

    Created a ListView for our display. We still need to edit the UI stuff

--- app/src/main/java/tskaws/app/ListBeta.java
+++ app/src/main/java/tskaws/app/ListBeta.java
@@ -1,39 +1,66 @@
 package tskaws.app;
 
 import android.app.usage.UsageEvents;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListBeta extends AppCompatActivity {
 
+
+    ListView list;
+    String[] titles = new String[2];
+    String[] description = new String[2];
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_beta);
 
-        Application application = new Application();
+        titles[0]= "Ropes Course";
+        description[0] = "Description";
 
-        List<EventItem> eventList;
+        titles[1]= "Date Night";
+        description[1] = "Description";
 
-        eventList = application.getEventItems();
+        list = (ListView) findViewById(R.id.events);
+        ListBeta.MyAdapter adapter = new ListBeta.MyAdapter(this, titles, description);
+        list.setAdapter(adapter);
 
-        ListView mListView;
+    }
 
-        mListView = (ListView) findViewById(R.id.theList);
+    class MyAdapter extends ArrayAdapter<String> {
+        Context context;
+        String myTitles[];
+        String myDescription[];
 
-        // Populate the list view
-        String[] listItems = new String[eventList.size()];
-
-        for(int i = 0; i < eventList.size(); i++){
-            listItems[i] =  eventList.get(i).getTitle();
+        MyAdapter(Context c, String[] titles, String[] description) {
+            super(c, R.layout.row, R.id.text1, titles);
+            this.context = c;
+            this.myTitles = titles;
+            this.myDescription = description;
         }
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
-        mListView.setAdapter(adapter);
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+            TextView myTitle = (TextView) row.findViewById(R.id.text1);
+            TextView myDescription = (TextView) row.findViewById(R.id.text2);
+            myTitle.setText(titles[position]);
+            myDescription.setText(description[position]);
+
+            return row;
+        }
     }
 }
--- app/src/main/java/tskaws/app/MainActivity.java
+++ app/src/main/java/tskaws/app/MainActivity.java
@@ -5,10 +5,20 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView list;
+    String[] titles = new String[2];
+    String[] description = new String[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().setElevation(0);
         Application app = Application.restore();
+
+        // Testing Dummies
+        titles[0]= "Ropes Course";
+        description[0] = "Description";
+        titles[1]= "Date Night";
+        description[1] = "Description";
+
+        // Populate the list
+        list = (ListView) findViewById(R.id.events);
+        MainActivity.MyAdapter adapter = new MainActivity.MyAdapter(this, titles, description);
+        list.setAdapter(adapter);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -30,6 +51,33 @@ public class MainActivity extends AppCompatActivity {
         searchView.setIconifiedByDefault(false);
         */
         return true;
+    }
+
+    // Using the row.xml layout create an adapter for the strings to
+    // adapt the list for display
+    class MyAdapter extends ArrayAdapter<String> {
+        Context context;
+        String myTitles[];
+        String myDescription[];
+
+        MyAdapter(Context c, String[] titles, String[] description) {
+            super(c, R.layout.row, R.id.text1, titles);
+            this.context = c;
+            this.myTitles = titles;
+            this.myDescription = description;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+            TextView myTitle = (TextView) row.findViewById(R.id.text1);
+            TextView myDescription = (TextView) row.findViewById(R.id.text2);
+            myTitle.setText(titles[position]);
+            myDescription.setText(description[position]);
+
+            return row;
+        }
     }
 }
 
